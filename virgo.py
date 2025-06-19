789f0cf810561177bce3cbbb9329f7b098bab3a4
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-06-20 01:13:47 +0800

    fix: cli command for creating application

--- virgo.py
+++ virgo.py
@@ -3,12 +3,14 @@ import os
 from virgo.core.lightserver import serve
 
 
-def start_project(project_name):
+def make_app(project_name):
     os.makedirs(project_name, exist_ok=True)
 
+    # __init__.py
     with open(os.path.join(project_name, "__init__.py"), "w") as f:
         f.write("")
 
+    # routes.py
     with open(os.path.join(project_name, "routes.py"), "w") as f:
         f.write(f"""from virgo.core.routing import routes
 from virgo.core.lightserver import Response
@@ -18,7 +20,9 @@ def home(request):
 
 routes["/"] = home
 """)
-        print(f"Project '{project_name}' created successfully.")
+
+    print(f"✅ App '{project_name}' created successfully.")
+
 
 if __name__ == "__main__":
     command = sys.argv[1] if len(sys.argv) > 1 else ""
@@ -26,11 +30,12 @@ if __name__ == "__main__":
     if command == "lightserve":
         import test.routes
         serve()
-    elif command == "lightstart":
-        if len(sys.argv) < 3:
-            print("Usage: py virgo.py lightstart <project_name>")
-        else:
-            start_project(sys.argv[2])
+    elif command.startswith("make:app"):
+        try:
+            project_name = sys.argv[2]
+            make_app(project_name)
+        except IndexError:
+            print("Usage: py virgo.py make:app <project_name>")
 
 
     else:
