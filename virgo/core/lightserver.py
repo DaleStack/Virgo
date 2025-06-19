6917fa0327164f8bd616e71ccf0b334296d19d19
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-06-20 00:39:29 +0800

    setting up lightserver to use my routes

--- virgo/core/lightserver.py
+++ virgo/core/lightserver.py
@@ -1,12 +1,29 @@
 from wsgiref.simple_server import make_server
+from virgo.core.routing import routes
+
+class Request:
+    def __init__(self, environ):
+        self.method = environ["REQUEST_METHOD"]
+        self.path = environ["PATH_INFO"]
+
+class Response:
+    def __init__(self, body, status="200 OK", content_type="text/html"):
+            self.body = body.encode("utf-8")
+            self.status = status
+            self.headers = [("Content-Type", content_type)]
+        
 
 def app(environ, start_response):
-    response_body = b"Hello from Virgo!"
-    status = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
+    request = Request(environ)
+    view_func = routes.get(request.path)
+    
+    if view_func:
+         response = view_func(request)
+    else:
+         response = Response("404 Not Found", status="404 Not Found")
 
-    start_response(status, headers)
-    return [response_body]
+    start_response(response.status, response.headers)
+    return [response.body]
 
 def serve():
     with make_server('', 8000, app) as httpd:
