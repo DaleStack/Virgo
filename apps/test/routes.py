38081fad810468044c8cf08162500fbe16890a4c
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-06-20 13:33:45 +0800

    fix: coding structure for routes file

--- apps/test/routes.py
+++ apps/test/routes.py
@@ -5,11 +5,10 @@ from virgo.core.template import render
 
 def show_post(request, id):
     return Response(f"Showing post #{id}")
+routes["/post/<int:id>"] = show_post
 
 def show_user(request, name):
     return Response(f"Hello {name}!")
-
-routes["/post/<int:id>"] = show_post
 routes["/user/<str:name>"] = show_user
 
 
