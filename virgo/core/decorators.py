3db72dd16de5ff3b74b0572f98f1801741dd21be
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-06-27 21:59:48 +0800

    feat: decorator for role-based routing

--- virgo/core/decorators.py
+++ virgo/core/decorators.py
@@ -1,6 +1,6 @@
 from virgo.core.auth import get_user
 from virgo.core.response import redirect
-from settings import LOGIN_ROUTE
+from settings import LOGIN_ROUTE, FORBIDDEN_REDIRECT_ROUTE
 
 def login_required(UserModel):
     def decorator(view_func):
@@ -11,4 +11,14 @@ def login_required(UserModel):
             request.user = user  # Attach user to request
             return view_func(request, *args, **kwargs)
         return wrapped
+    return decorator
+
+def role_required(role):
+    def decorator(view_func):
+        def wrapper(request, *args, **kwargs):
+            user = get_user(request)
+            if not user or getattr(user, "role", None) != role:
+                return redirect(FORBIDDEN_REDIRECT_ROUTE)
+            return view_func(request, *args, **kwargs)
+        return wrapper
     return decorator
